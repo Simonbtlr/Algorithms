d3c1a68ec71037b95dbbc711138d8c9638b494d1
simon <prog@dowell.ru>
2022-07-05 18:40:39 +0300

    Binary Search implement

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Algorithms.Search;
 
-Console.WriteLine("Hello, World!");
+namespace Algorithms;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var list = new List<int>
+        {
+            1, 3, 5, 7, 9
+        };
+        
+        Console.WriteLine(new BinarySearch().Search(list, 3));  // 1
+        Console.WriteLine(new BinarySearch().Search(list, -1)); // 0
+    }
+}
---
+++ Algorithms/Search/BinarySearch.cs
@@ -0,0 +1,26 @@
+namespace Algorithms.Search;
+
+public class BinarySearch
+{
+    public int Search (List<int> list, int item)
+    {
+        var low = 0;
+        var high = list.Count;
+
+        while (low <= high)
+        {
+            var mid = (low + high) / 2;
+            var guess = list[mid];
+
+            if (guess == item)
+                return mid;
+            
+            if (guess > item)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return 0;
+    }
+}
